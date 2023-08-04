76b1017669b0609193695ad140328e2e021b6daf
Emhyrr <118763844+Emhyrr@users.noreply.github.com>
2023-08-04 21:41:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import load_img, img_to_array
 import os
 
 app = Flask(__name__, template_folder='.')
